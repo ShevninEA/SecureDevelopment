a9f9e90d75313e9d1f298badf38ca216020fa580
ShevninEA <ev.shevnin@yandex.ru>
2023-02-17 18:24:14 +0300

    Добавьте файлы проекта.

---
+++ SecureDevelopment/Controllers/CardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureDevelopment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        private readonly ILogger<CardController> _logger;
+
+        public CardController(ILogger<CardController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
